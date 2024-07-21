f7c445af82a358c08da64098a3c5bd73f1e54499
Lucas Chen <130620394+lucasch37@users.noreply.github.com>
2024-07-21 02:14:56 -0700

    asdlkjakfj

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ const nextConfig = {
     images: {
         domains: ["lh3.googleusercontent.com", "avatars.githubusercontent.com"],
     },
+    typescript: {
+        ignoreBuildErrors: true,
+    },
 };
 
 export default nextConfig;
